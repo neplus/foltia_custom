526466b58a4ff2f2af7196cebce29cf9ace36220
Yuichi.Shiwaku <yuichi.shiwaku@gmail.com>
2011-04-11 02:17:38 +0900

    add cron file

---
+++ perl/cron_foltia_dayly.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Anime recording system foltia
+# http://www.dcc-jpl.com/soft/foltia/
+#
+# 定期実行ジョブ記述ファイル。
+#cronで1日1回程度実行するとよいでしょう。
+#
+# DCC-JPL Japan/foltia project
+#
+
+#デジタル放送から一週間分のEPGを取得
+#/home/foltia/perl/epgimport.pl long
+/tv/perl/epgimport.pl long
+
+# XMLTVをつかってEPG番組表インポート(アナログ専用旧仕様)
+#
+#/usr/bin/perl  /usr/bin/tv_grab_jp | /home/foltia/perl/xmltv2foltia.pl
+# 2つの局設定使うような場合
+#/usr/bin/perl  /usr/bin/tv_grab_jp --config-file ~/.xmltv/tv_grab_jp.conf.jcom  | /home/foltia/perl/xmltv2foltia.pl
+#/usr/bin/perl  /usr/bin/tv_grab_jp --config-file ~/.xmltv/tv_grab_jp.conf.tvk  | /home/foltia/perl/xmltv2foltia.pl
+
+#録画ファイルとテーブルの整合性を更新
+/tv/perl/updatem2pfiletable.pl
+
+#2週間先のスケジュールを取得
+/tv/perl/getxml2db.pl long
+
