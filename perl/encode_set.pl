2917607e7dcfc6667567bc4b24764763777c99fc
Yuichi.Shiwaku <yuichi.shiwaku@gmail.com>
2011-04-13 03:12:21 +0900

    encode_set script changed.

--- perl/encode_set.pl
+++ perl/encode_set.pl
@@ -5,17 +5,26 @@
 #
 #
 use DBI;
-use DBD::Pg;
+#use DBD::Pg;
 use DBD::SQLite;
 use Jcode;
 
-use lib "/tv/perl";
+#use lib "/tv/perl";
 require "foltialib.pl";
 
+$dbh=DBI->connect($DSN, $DBUser, $DBPass)||die $DBI::error;
+$sth=$dbh->prepare('select m2pfilename from foltia_subtitle where filestatus=999');
+$sth->execute();
+
+while(my $ref = $sth->fetch ){
+	&encode_set(@_)
+}
+
+
 sub encode_set{
-	($status, $filename) = @_;
+	($filename) = @_;
 	$dbh=DBI->connect($DSN,$DBUser,$DBPass)||die $DBI::error;
 	$sth=$dbh->prepare('update foltia_subtitle set filestatus=? WHERE foltia_subtitle.m2pfilename = ?');
-	$sth->execute($status, $filename);
+	$sth->execute($filename);
 }
 
